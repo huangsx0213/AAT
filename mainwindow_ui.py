b2ebe8da708bf72fc3093e556af8c7070f7ab05a
vhuang1 <vick.huang@quest.com>
2017-09-28 22:21:21 +0800

    added setup_execution_tab

--- mainwindow_ui.py
+++ mainwindow_ui.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QGridLayout, QToolBox, QLabel, QGroupBox
 
 
 class MainWindow_Ui(object):
@@ -24,3 +25,33 @@ class MainWindow_Ui(object):
         self.action_tab = QtWidgets.QWidget()
         self.action_tab.setObjectName("tab2")
         self.addTab(self.action_tab, "Action")
+
+        self.setup_execution_tab()
+
+    def setup_execution_tab(self):
+        # define the QGridLayout execution_tab_layout into the execution_tab.
+        self.execution_tab_layout = QGridLayout()
+        self.execution_tab_layout.setContentsMargins(5, 5, 5, 5)
+        self.execution_tab.setLayout(self.execution_tab_layout)
+        # define the left groupbox
+        # 1. definea groupbox
+        self.left_groupbox = QGroupBox()
+        self.left_groupbox.setFixedWidth(185)
+        # 2.define a gridlayout
+        self.left_gridlayout = QGridLayout()
+        self.left_gridlayout.setContentsMargins(2, 2, 2, 2)
+        # 3.define a toolbox
+        self.page = QtWidgets.QWidget()
+        self.page.setObjectName("page")
+        self.left_toolbox = QToolBox()
+        self.left_toolbox.addItem(self.page, "Execution")
+
+        self.left_gridlayout.addWidget(self.left_toolbox)
+        self.left_groupbox.setLayout(self.left_gridlayout)
+
+        # define the right groupbox
+        self.left_groupbox2 = QGroupBox()
+
+        # add the left groupbox and  right groupbox into execution_tab_layout
+        self.execution_tab_layout.addWidget(self.left_groupbox, 0, 0)
+        self.execution_tab_layout.addWidget(self.left_groupbox2, 0, 1)
