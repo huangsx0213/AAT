c336984ea028f4f11f6188a831e8c77f1d594137
vhuang1 <vick.huang@quest.com>
2017-10-24 17:11:19 +0800

    Update testset ui and logic

--- UserInterface/Execution_UI.py
+++ UserInterface/Execution_UI.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QCursor
 from PyQt5.QtWidgets import QGridLayout, QGroupBox, QToolBox, QAction, QTabBar, QVBoxLayout, QLabel, QLineEdit, \
-    QPushButton
+    QPushButton, QSplitter
 
 from UserInterface.CustomGroupBox import CustomGroupBox
 
@@ -53,8 +53,8 @@ class Execution_UI(object):
         self.execution_groupbox = QGroupBox()
         self.execution_groupbox.setContentsMargins(0, 0, 0, 0)
         # 2.define a gridlayout
-        self.execution_action_gridlayout = QGridLayout()
-        self.execution_action_gridlayout.setContentsMargins(3, 2, 1, 3)
+        self.execution_gridlayout = QGridLayout()
+        self.execution_gridlayout.setContentsMargins(3, 2, 1, 3)
         # 3.define a tabwidget
         # first page of the tabwidget
         self.all_executions_tab = QtWidgets.QWidget()
@@ -74,10 +74,10 @@ class Execution_UI(object):
         QTabBar.setTabButton(self.execution_tabwidget.tabBar(), 0, QTabBar.RightSide, None)
 
         # add the ex_right_content_toolbar and  ex_right_content_tabwidget into gridlayout
-        self.execution_action_gridlayout.addWidget(self.execution_main_tab_toolbar, 0, 0)
-        self.execution_action_gridlayout.addWidget(self.execution_tabwidget, 1, 0)
+        self.execution_gridlayout.addWidget(self.execution_main_tab_toolbar, 0, 0)
+        self.execution_gridlayout.addWidget(self.execution_tabwidget, 1, 0)
         # add the gridlaout into the groupbox
-        self.execution_groupbox.setLayout(self.execution_action_gridlayout)
+        self.execution_groupbox.setLayout(self.execution_gridlayout)
 
         # add the left groupbox and  right groupbox into execution_tab_layout
         self.execution_main_tab_layout.addWidget(self.execution_menu_groupbox, 0, 0)
@@ -95,17 +95,24 @@ class Execution_UI(object):
 
         # add tableview into execution
         self.execution_tableview = QtWidgets.QTableView()
-        self.execution_action_gridlayout = QGridLayout()
-        self.execution_action_gridlayout.setContentsMargins(0, 0, 0, 0)
-        self.all_executions_tab.setLayout(self.execution_action_gridlayout)
-        self.execution_action_gridlayout.addWidget(self.execution_tableview)
+        self.execution_gridlayout = QGridLayout()
+        self.execution_gridlayout.setContentsMargins(0, 0, 0, 0)
+        self.all_executions_tab.setLayout(self.execution_gridlayout)
+        self.execution_gridlayout.addWidget(self.execution_tableview)
 
         # add tableview into execution
         self.testset_tableview = QtWidgets.QTableView()
+        self.testset_tableview.setBaseSize(303,500)
+        self.testset_testcase_tableview = QtWidgets.QTableView()
         self.testset_gridlayout = QGridLayout()
         self.testset_gridlayout.setContentsMargins(0, 0, 0, 0)
         self.all_testset_tab.setLayout(self.testset_gridlayout)
-        self.testset_gridlayout.addWidget(self.testset_tableview)
+        self.all_testset_splitter=QSplitter(Qt.Horizontal)
+        self.testset_gridlayout.addWidget(self.all_testset_splitter)
+        self.all_testset_splitter.addWidget(self.testset_tableview)
+        self.all_testset_splitter.addWidget(self.testset_testcase_tableview)
+        self.all_testset_splitter.setStretchFactor(0,60)
+        self.all_testset_splitter.setStretchFactor(1,40)
 
     def setup_dynamic_excution_tab(self, name=None):
         # 1. define a page
@@ -170,12 +177,45 @@ class Execution_UI(object):
         self.execution_details_gridlayout.addWidget(self.execution_details_state_lineedit, 3, 1)
         self.execution_details_gridlayout.addWidget(self.execution_details_row_lineedit, 4, 0)
 
+    def setup_dynamic_testset_tab(self, name=None):
+        # 1. define a page
+        self.one_dynamic_testset_tab = QtWidgets.QWidget()
+        self.one_dynamic_testset_tab.setObjectName(name)
+        # 2. define a layout
+        self.one_dynamic_testset_layout = QVBoxLayout()
+        # self.ex_right_content_one_ex_layout.setSizeConstraint(QLayout.SetNoConstraint)
+        # 3. define a Groupbox
+        self.testset_details_groupbox = CustomGroupBox("Details")
+        # self.ex_right_content_ex_details_groupbox.setChecked(False)
+        # 4. add the page to the tabwidet
+        self.execution_tabwidget.addTab(self.one_dynamic_testset_tab, name)
+        self.execution_tabwidget.setCurrentWidget(self.one_dynamic_testset_tab)
+        # 5.set layout to the page tab
+        self.one_dynamic_testset_tab.setLayout(self.one_dynamic_testset_layout)
+        # 6. add all groupbox into page tab layout
+        self.one_dynamic_testset_layout.addWidget(self.testset_details_groupbox)
+        self.one_dynamic_testset_layout.addStretch()
+        self.testset_details_gridlayout = QGridLayout()
+        self.testset_details_groupbox.setLayout(self.testset_details_gridlayout)
+
+        # Details groupbox
+        self.testset_details_name_lable = QLabel("Name:")
+        self.testset_details_name_lineedit = QLineEdit()
+        self.testset_details_name_lineedit.setObjectName("testset_name")
+
+        self.testset_details_row_lineedit = QLineEdit()
+        self.testset_details_row_lineedit.setObjectName("row")
+        self.testset_details_row_lineedit.hide()
+        self.testset_details_gridlayout.addWidget(self.testset_details_name_lable, 0, 0)
+        self.testset_details_gridlayout.addWidget(self.testset_details_name_lineedit, 0, 1)
+        self.testset_details_gridlayout.addWidget(self.testset_details_row_lineedit, 1, 0)
+        
     def setup_execution_actions_column(self):
         self.execution_action_widget = QtWidgets.QWidget()
         self.execution_action_widget.setContentsMargins(0, 0, 0, 0)
-        self.execution_action_gridlayout = QGridLayout()
-        self.execution_action_gridlayout.setContentsMargins(3, 3, 3, 3)
-        self.execution_action_widget.setLayout(self.execution_action_gridlayout)
+        self.execution_gridlayout = QGridLayout()
+        self.execution_gridlayout.setContentsMargins(3, 3, 3, 3)
+        self.execution_action_widget.setLayout(self.execution_gridlayout)
         self.execution_edit_button = QPushButton()
         self.execution_edit_button.setObjectName("edit")
         self.execution_edit_button.setIcon(QIcon("./images/edit.png"))
@@ -188,5 +228,26 @@ class Execution_UI(object):
         self.execution_delete_button.setFlat(True)
         self.execution_delete_button.setCursor(QCursor(Qt.PointingHandCursor))
         self.execution_delete_button.setToolTip("Delete")
-        self.execution_action_gridlayout.addWidget(self.execution_edit_button, 0, 0)
-        self.execution_action_gridlayout.addWidget(self.execution_delete_button, 0, 1)
+        self.execution_gridlayout.addWidget(self.execution_edit_button, 0, 0)
+        self.execution_gridlayout.addWidget(self.execution_delete_button, 0, 1)
+        
+    def setup_testset_actions_column(self):
+        self.testset_action_widget = QtWidgets.QWidget()
+        self.testset_action_widget.setContentsMargins(0, 0, 0, 0)
+        self.testset_action_gridlayout = QGridLayout()
+        self.testset_action_gridlayout.setContentsMargins(3, 3, 3, 3)
+        self.testset_action_widget.setLayout(self.testset_action_gridlayout)
+        self.testset_edit_button = QPushButton()
+        self.testset_edit_button.setObjectName("testset_edit")
+        self.testset_edit_button.setIcon(QIcon("./images/edit.png"))
+        self.testset_edit_button.setFlat(True)
+        self.testset_edit_button.setCursor(QCursor(Qt.PointingHandCursor))
+        self.testset_edit_button.setToolTip("Edit")
+        self.testset_delete_button = QPushButton()
+        self.testset_delete_button.setObjectName("testset_edit")
+        self.testset_delete_button.setIcon(QIcon("./images/delete.png"))
+        self.testset_delete_button.setFlat(True)
+        self.testset_delete_button.setCursor(QCursor(Qt.PointingHandCursor))
+        self.testset_delete_button.setToolTip("Delete")
+        self.testset_action_gridlayout.addWidget(self.testset_edit_button, 0, 0)
+        self.testset_action_gridlayout.addWidget(self.testset_delete_button, 0, 1)
