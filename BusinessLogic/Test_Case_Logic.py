9099e372c5e79b10bddb10cb80a99ba41685da61
vhuang1 <vick.huang@quest.com>
2018-04-09 15:58:10 +0800

    Test case tree logic

---
+++ BusinessLogic/Test_Case_Logic.py
@@ -0,0 +1,7 @@
+from UserInterface.MainWindow_UI import MainWindow_UI
+
+class Test_Case_Logic(MainWindow_UI):
+    def test_case_logic(self):
+
+        # test case table logic
+        self.setup_dynamic_test_case_tab("test")
